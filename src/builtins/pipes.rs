2d4eb38cf1eee17d2753476e3d07fb3985b121de
Saksham Mittal <gotlouemail@gmail.com>
2023-09-13 11:00:49 +0530

    feat: allow pipe syscalls in Pipes ruleset

---
+++ src/builtins/pipes.rs
@@ -0,0 +1,22 @@
+//! Contains a [`RuleSet`] for allowing pipes
+
+use std::collections::HashMap;
+use syscalls::Sysno;
+use crate::{SeccompRule, RuleSet};
+
+/// [`Pipes`] allows you to create anonymous pipes for inter-process communication via the `pipe`
+/// syscalls.
+pub struct Pipes;
+impl RuleSet for Pipes {
+    fn simple_rules(&self) -> Vec<Sysno> {
+        vec![Sysno::pipe, Sysno::pipe2]
+    }
+
+    fn conditional_rules(&self) -> HashMap<Sysno, Vec<SeccompRule>> {
+        HashMap::new()
+    }
+
+    fn name(&self) -> &'static str {
+        "Pipes"
+    }
+}
